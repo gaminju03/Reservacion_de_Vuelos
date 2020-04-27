aa8762c0fb9a00f457e7ab9bedf33fda8492f3b5
Juan Gamino <gamino_juan03@hotmail.com>
2020-04-27 01:57:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/reservacion_de_vuelos/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.reservacion_de_vuelos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.registro_vuelo.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    //Creamos los item para usar en el spiner de pago
+    var lista_de_pagos = arrayOf("Efectivo", "Tarjeta", "Cupon")
+    var origen = arrayOf("Mexico", "Estados Unidos", "China","Japon","Africa")
+    var destino = arrayOf("Mexico", "Estados Unidos", "China","Japon","Africa")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        //Crea un areglo para usar el simple sninner de pago
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista_de_pagos)
+
+        // seleccionamos el modo para que aparesca en el diseño
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // mostramos en el spinner los item
+      //  spinner!!.setAdapter(aa)
+
+
+
+        //Crea un areglo para usar el simple sninner de Origen
+        val oo = ArrayAdapter(this, android.R.layout.simple_spinner_item, origen)
+
+        // seleccionamos el modo para que aparesca en el diseño
+        oo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // mostramos en el spinner los item
+      //  spinner2!!.setAdapter(oo)
+
+
+        //Crea un areglo para usar el simple sninner de Destino
+        val dd = ArrayAdapter(this, android.R.layout.simple_spinner_item, destino)
+
+        // seleccionamos el modo para que aparesca en el diseño
+        dd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // mostramos en el spinner los item
+     //   spndestino!!.setAdapter(dd)
+
+
+    }
+
+
+
+}
+
